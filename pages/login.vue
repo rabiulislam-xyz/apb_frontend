49d70e000971018e3126a9a5e478a4c445c8059a
Rabiul Islam <rabiulislam993@live.com>
2021-11-01 07:20:06 +0600

    added back button

--- pages/login.vue
+++ pages/login.vue
@@ -1,39 +1,54 @@
 <template>
-  <!-- <section class="section">
-    <b-button @click="loginWithGoogle()" icon-left="google">
-      Sign in with Google
-    </b-button>
-  </section> -->
   <div>
-    <button
+    <nuxt-link to="/">
+      <button class="text-black bg-white hover:bg-gray-100">Back</button>
+    </nuxt-link>
+    <div
       class="
-        uppercase
-        h-12
-        mt-3
-        text-white
+        pt-4
+        bg-white
         w-full
-        rounded
-        bg-blue-800
-        hover:bg-blue-900
+        md:w-56
+        justify-center
+        items-center
+        shadow
+        px-6
+        py-4
+        flex flex-col
       "
     >
-      <i class="fa fa-facebook mr-2"></i>Facebook
-    </button>
-    <button
-      @click="loginWithGoogle()"
-      class="
-        uppercase
-        h-12
-        mt-3
-        text-white
-        w-full
-        rounded
-        bg-red-800
-        hover:bg-red-900
-      "
-    >
-      <i class="fa fa-google mr-2"></i>Google
-    </button>
+      Login with
+
+      <button
+        class="
+          uppercase
+          h-12
+          mt-3
+          text-white
+          w-full
+          rounded
+          bg-blue-800
+          hover:bg-blue-900
+        "
+      >
+        <i class="fa fa-facebook mr-2"></i>Facebook
+      </button>
+      <button
+        @click="loginWithGoogle()"
+        class="
+          uppercase
+          h-12
+          mt-3
+          text-white
+          w-full
+          rounded
+          bg-red-800
+          hover:bg-red-900
+        "
+      >
+        <i class="fa fa-google mr-2"></i>Google
+      </button>
+    </div>
   </div>
 </template>
 
