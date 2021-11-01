4fd26e9bd8b1f39e4466c2c314110b07f40f549a
Rabiul Islam <rabiulislam993@live.com>
2021-11-01 07:06:42 +0600

    added login link

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <Tutorial/>
+  <a class="hover:bg-gray-200 text-black text-center py-2 px-4 rounded">
+    <nuxt-link to="/login">click here to login</nuxt-link>
+  </a>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
